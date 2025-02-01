9950d7c79bcfb6be47abdbe4771983044a528bdc
jay <maestro1286@gmail.com>
2025-02-01 08:42:46 -0800

    add test site and server

---
+++ server.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+
+# Simple test server to test web crawlser against
+
+
+def get_file(filename: str) -> str:
+    with open(os.path.join("test-site", filename), mode="r") as f:
+        content = f.read()
+    return content
+
+
+@app.route("/robots.txt")
+def robots_txt():
+    content = get_file("robots.txt")
+    return Response(content, mimetype="text/plain")
+
+
+@app.route("/sitemap.xml")
+def sitemap_xml():
+    content = get_file("sitemap.xml")
+    return Response(content, mimetype="application/xml")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
