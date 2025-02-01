7940acc8162187d9f72acacb19746bd2dc4dd65b
jay <maestro1286@gmail.com>
2025-02-01 14:42:40 -0800

    update robotstxt parser

--- robots.py
+++ robots.py
@@ -1,3 +1,4 @@
+import re
 import httpx
 from urllib.parse import urljoin
 
@@ -11,6 +12,8 @@ class RobotsTxtHandler:
         """
         self.base_url = base_url.rstrip("/") + "/"  # Ensure trailing slash
         self.robots_url = urljoin(self.base_url, "robots.txt")
+        self.sitemap_url = urljoin(self.base_url, "sitemap.xml")
+        self.discovered_urls = []
         self.timeout = timeout
         self.content = None
         self.rules = {}
@@ -23,18 +26,19 @@ class RobotsTxtHandler:
         self.close()
 
     def fetch(self) -> None:
-        """Fetches the robots.txt file from the website and stores it in memory."""
+        """Fetches the robots.txt file and sitemap.xml file from the website and store them in memory."""
         try:
             response = self.client.get(self.robots_url)
             if response.status_code == 200:
                 self.content = response.text
-                self._parse()
+                self._parse_robots_file()
+                self._parse_sitemap()
             else:
-                print(f"Failed to retrieve robots.txt: {response.status_code}")
+                print(f"[-] failed to retrieve robots.txt: {response.status_code}")
         except httpx.RequestError as e:
-            print(f"Request error: {e}")
+            print(f"[-] request error: {e}")
 
-    def _parse(self) -> None:
+    def _parse_robots_file(self) -> None:
         """Parses the robots.txt file and stores the rules in a dictionary."""
         self.rules.clear()
         if not self.content:
@@ -54,9 +58,44 @@ class RobotsTxtHandler:
             if key == "user-agent":
                 user_agent = value
                 if user_agent not in self.rules:
-                    self.rules[user_agent] = {"allow": [], "disallow": []}
+                    self.rules[user_agent] = {
+                        "allow": [],
+                        "disallow": [],
+                        "sitemap": "",
+                    }
             elif key in ("disallow", "allow") and user_agent:
                 self.rules[user_agent][key].append(value)
+            elif key == "sitemap":
+                self.rules[key] = value
+
+    def _parse_sitemap(self) -> list:
+        data = None
+        try:
+            response = self.client.get(self.sitemap_url)
+            if response.status_code == 200:
+                data = response.text
+        except httpx.RequestError as e:
+            print(f"[-] request error: {e}")
+
+        if not data:
+            return []
+        urls = []
+        url_pattern = re.compile(
+            r"<url>.*?<loc>(.*?)</loc>.*?(<lastmod>(.*?)</lastmod>)?.*?(<changefreq>(.*?)</changefreq>)?.*?(<priority>(.*?)</priority>)?.*?</url>",
+            re.DOTALL,
+        )
+        matches = url_pattern.findall(data)
+        for match in matches:
+            urls.append(
+                {
+                    "loc": match[0],
+                    "lastmod": match[2] if match[2] else None,
+                    "changefreq": match[4] if match[4] else None,
+                    "priority": float(match[6]) if match[6] else None,
+                }
+            )
+
+        self.discovered_urls = urls
 
     def is_allowed(self, user_agent: str, path: str) -> bool:
         """Checks if a given path is allowed for the specified user-agent."""
@@ -79,6 +118,13 @@ class RobotsTxtHandler:
             user_agent.lower(), self.rules.get("*", {"allow": [], "disallow": []})
         )
 
+    def get_sitemap_urls(self) -> list:
+        """Returns the list of urls found when parsing the sitemap.xml file"""
+        urls = [
+            self.discovered_urls["loc"] for self.discovered_urls in self.discovered_urls
+        ]
+        return urls
+
     def close(self):
         """Closes the HTTP client session."""
         self.client.close()
@@ -86,8 +132,9 @@ class RobotsTxtHandler:
 
 # Example usage:
 if __name__ == "__main__":
-    robots_client = RobotsTxtHandler("https://example.com")
+    robots_client = RobotsTxtHandler("http://localhost:8000")
     robots_client.fetch()
     print("Rules for '*':", robots_client.get_rules("*"))
+    print("Site urls from sitemap:", ", ".join(robots_client.get_sitemap_urls()))
     print("Can we crawl '/private'?:", robots_client.is_allowed("mybot", "/private"))
     robots_client.close()
